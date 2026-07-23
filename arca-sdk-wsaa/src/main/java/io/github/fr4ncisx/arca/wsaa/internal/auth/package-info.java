cdf8193fe3171cba1510c8baa5e9a1fd713b0bec
fr4ncisx <fr4ncisx@gmail.com>
2026-07-23 08:03:21 -0300

    chore(wsaa): add @NullMarked package-info to auth and certificate packages
    Add package-info.java with @NullMarked to wsaa internal.auth and
    internal.certificate packages for complete null-safety coverage.

---
+++ arca-sdk-wsaa/src/main/java/io/github/fr4ncisx/arca/wsaa/internal/auth/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * WSAA internal authentication implementations.
+ * All types in this package are null-marked by default.
+ */
+@NullMarked
+package io.github.fr4ncisx.arca.wsaa.internal.auth;
+
+import org.jspecify.annotations.NullMarked;
---
+++ arca-sdk-wsaa/src/main/java/io/github/fr4ncisx/arca/wsaa/internal/certificate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * WSAA certificate handling implementations.
+ * All types in this package are null-marked by default.
+ */
+@NullMarked
+package io.github.fr4ncisx.arca.wsaa.internal.certificate;
+
+import org.jspecify.annotations.NullMarked;
