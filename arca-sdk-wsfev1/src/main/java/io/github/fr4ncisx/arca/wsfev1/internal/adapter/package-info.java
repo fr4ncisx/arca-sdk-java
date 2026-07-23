ec4888d6a62b2d4abf94622e7c5572fdeb3472c8
fr4ncisx <fr4ncisx@gmail.com>
2026-07-23 08:01:30 -0300

    chore(wsfev1): add @NullMarked package-info to adapter layer
    Add package-info.java with @NullMarked annotation to the newly created
    internal.adapter package for compile-time null-safety analysis.

---
+++ arca-sdk-wsfev1/src/main/java/io/github/fr4ncisx/arca/wsfev1/internal/adapter/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * WSFEv1 adapter layer mapping raw SOAP stubs to domain model records.
+ * All types in this package are null-marked by default.
+ */
+@NullMarked
+package io.github.fr4ncisx.arca.wsfev1.internal.adapter;
+
+import org.jspecify.annotations.NullMarked;
